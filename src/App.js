903fa849b0003b59a004a9a232a827e89f63a823
CamMeiners <cammeiners@gmail.com>
2021-09-08 16:16:44 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import { BASE_URL, API_KEY} from './constants';
+
 
 function App() {
+  const [picture, setPicture] = useState({})
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then(res =>{
+      setPicture(res.data);
+      console.log(res.data);
+    }).catch(err => console.error(err));
+  },[]) 
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>{'Pictures That Are Out of This World!'}</h1>
+      <img src={picture.url}/>
+      <div>
+      <h2>{picture.date}</h2>
+      </div>
+      <div>
+        <h3>{picture.explanation}</h3>
+      </div>
     </div>
+    
+      
   );
 }
 
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'jbHIL7PjBHm6gpNUQuZZ3ahJtp6Btkz2MK44fghl'
